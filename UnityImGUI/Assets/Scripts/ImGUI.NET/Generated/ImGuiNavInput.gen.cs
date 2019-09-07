473824d9f96df95079f2b57912c45b7a6480eb42
Jay X Peet <jaytechmedia@googlemail.com>
2019-09-07 19:17:31 +0100

    [^] Added Imgui.Net, with correct asmdef.

---
+++ UnityImGUI/Assets/Scripts/ImGUI.NET/Generated/ImGuiNavInput.gen.cs
@@ -0,0 +1,30 @@
+namespace ImGuiNET
+{
+    public enum ImGuiNavInput
+    {
+        Activate = 0,
+        Cancel = 1,
+        Input = 2,
+        Menu = 3,
+        DpadLeft = 4,
+        DpadRight = 5,
+        DpadUp = 6,
+        DpadDown = 7,
+        LStickLeft = 8,
+        LStickRight = 9,
+        LStickUp = 10,
+        LStickDown = 11,
+        FocusPrev = 12,
+        FocusNext = 13,
+        TweakSlow = 14,
+        TweakFast = 15,
+        KeyMenu = 16,
+        KeyTab = 17,
+        KeyLeft = 18,
+        KeyRight = 19,
+        KeyUp = 20,
+        KeyDown = 21,
+        COUNT = 22,
+        InternalStart = KeyMenu,
+    }
+}
